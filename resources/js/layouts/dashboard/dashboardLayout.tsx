076c129dbbe70e40cdd498365fa987dc7b90bf9d
Elsabeth721 <elsabethzeleke1000@gmail.com>
2025-05-01 08:33:26 +0300

    added common layout

---
+++ resources/js/layouts/dashboard/dashboardLayout.tsx
@@ -0,0 +1,16 @@
+import type { ReactNode } from "react"
+import { SidebarProvider } from "@/components/ui/sidebar"
+import { DashboardHeader } from "@/components/dashboard/studentDashboard/DashboardHeader"
+import { Sidebar } from "@/components/dashboard/studentDashboard/Sidebar";
+
+export function AppLayout({ children, title }: { children: ReactNode; title?: string }) {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="dashboard-content flex-1 flex flex-col w-full max-w-full overflow-x-hidden">
+        <DashboardHeader title={title} />
+        <main className="flex-1 p-6 overflow-auto w-full max-w-full">{children}</main>
+      </div>
+    </div>
+  )
+}
